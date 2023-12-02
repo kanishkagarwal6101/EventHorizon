6a4ec5c0b4b6dc641c50fbef621b9817bc667693
Harsh Parikh <parihm01@pfw.edu>
2023-12-02 14:23:50 -0600

    QR generation with cors connections

--- Backend/routes/Event_Routes.js
+++ Backend/routes/Event_Routes.js
@@ -3,6 +3,5 @@ import { getAllEvents } from "../controllers/event-controller";
 
 const prodRouter = express.Router();
 prodRouter.get("/getAllEvents", getAllEvents);
-// prodRouter.post("/postEvents", getAllEvents);
 
 export default prodRouter;
