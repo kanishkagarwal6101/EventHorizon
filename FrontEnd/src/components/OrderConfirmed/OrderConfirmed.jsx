6884d2889f80de2ac51f289573d9e60990f4f1d6
Kanishk Agarwal <agark02@pfw.edu>
2023-12-04 01:30:26 -0500

    want backend changes

--- FrontEnd/src/components/OrderConfirmed/OrderConfirmed.jsx
+++ FrontEnd/src/components/OrderConfirmed/OrderConfirmed.jsx
@@ -1,14 +1,50 @@
 import React from "react";
 import { useContext } from "react";
 import Context from "../contextFiles/context";
+import { toString, toDataURL } from "qrcode";
+import QRCode from "react-qr-code";
+import { useEffect } from "react";
+import CardComponent from "../card/CardComponent";
 const OrderConfirmed = () => {
+  const [data, setData] = React.useState([]);
+  const [filteredData, setFilteredData] = React.useState([]);
+  var qrSrc = "";
   //   console.log(JSON.parse(localStorage.getItem("addInfo")));
   const allData = JSON.parse(localStorage.getItem("addInfo"));
   const name = allData.name;
   console.log(allData.selectedSeats);
+  const postData = {
+    name: name,
+    seats: allData.selectedSeats.join(","),
+  };
   //   const data = useContext(Context);
   // Function to generate QR code can be placed here
+  useEffect(() => {
+    console.log("hi");
+    const fetchData = () => {
+      fetch("http://127.0.0.1:8080/landingpage/getAllEvents")
+        .then((response) => response.json())
+        .then((d) => {
+          // console.log(d.events);
+          for (let event of d.events) {
+            setData((data) => [...data, event]);
+          }
+        });
+    };
 
+    fetch("", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ postData }),
+    });
+    fetchData();
+    console.log(data);
+
+    console.log(filteredData);
+  }, []);
+  //   console.log(filteredData);
   return (
     <div className="movie-order-confirmed">
       <h1>Order Confirmed</h1>
@@ -20,11 +56,15 @@ const OrderConfirmed = () => {
         </p>
         <p>Location: {allData.location}</p>
         <p>Seats: {allData.selectedSeats.join(", ")}</p>
-        {/* QR code component can go here */}
+        {/* <img src={svgContent} alt="QR code" /> */}
+        {/* <div className="svg" dangerouslySetInnerHTML={{ __html: svgContent }} /> */}
+        <QRCode
+          value={`${allData.name}, ${allData.selectedSeats}, ${allData.date}, ${allData.time}, ${allData.location}  `}
+        />
       </div>
       <div className="recommendation">
         <h2>You might also like</h2>
-        {/* Recommendation section body goes here */}
+        {/* <p>{filteredData[0].name}</p> */}
       </div>
     </div>
   );
